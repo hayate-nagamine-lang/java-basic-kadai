975bc010be5ed891e67d3435e5a5a5ce387027a9
hayate-nagamine-lang <hayate-nagamine@gr.paramix.co.jp>
2026-05-01 11:19:56 +0900

    java 課題15提出

---
+++ kadai_015/CarExec_Chapter.java
@@ -0,0 +1,19 @@
+package kadai_015;
+
+public class CarExec_Chapter {
+
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	// 車クラスのインスタンスの作成
+		Car_Chapter15 myCar = new Car_Chapter15();
+		
+		//車クラスのchangeGearメソッドを実行する
+		myCar.changeGear  (3);
+		
+		//車クラスのrunメソッドの実行
+		myCar.run();
+		
+ }
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,29 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	//フィールド（内部データ）
+	private int gear  = 1;  //1速から5速のギアを表す
+	private int speed = 10; //ギアチェンジ後の速度を表す
+	
+	//メソッド
+	public void changeGear(int afterGear) {
+		System.out.println("ギアを" + this.gear + "から" + afterGear + "に切り替えました" );
+		this.gear = afterGear;
+		
+		//ギアに応じた速度の設定
+		this.speed = switch(this.gear) {
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default ->10;
+		};
+	}	
+		public void run () {
+			System.out.println("速度は時速" + this.speed + "kmです");
+		}
+	}
+
+
